426fe02e55a7a5718be0712edb4a3aabafa7c230
Nikolay Yaroshenko <nikolayyaroshenko@MacBook-Pro-Nikolay.local>
2023-11-20 10:53:53 +0300

    14

--- src/channel.py
+++ src/channel.py
@@ -8,17 +8,58 @@ class Channel:
     YOUTUBE_API_VERSION = 'v3'
     all = []
 
-    def __init__(self, channel_id):
+    def __init__(self, channel_id, title='', description='', subscriber_count=0):
         self.channel_id = channel_id
         self.data = self.get_channel_data()
 
         self.id = self.channel_id
-        self.title = self.data['snippet']['title']
-        self.description = self.data['snippet']['description']
+        self.title = title or self.data['snippet']['title']
+        self.description = description or self.data['snippet']['description']
         self.url = f"https://www.youtube.com/channel/{self.channel_id}"
-        self.subscriber_count = self.data['statistics']['subscriberCount']
-        self.video_count = self.data['statistics']['videoCount']
-        self.view_count = self.data['statistics']['viewCount']
+        self.subscriber_count = subscriber_count or self.string_to_number(self.data['statistics']['subscriberCount'])
+        self.video_count = self.string_to_number(self.data['statistics']['videoCount'])
+        self.view_count = self.string_to_number(self.data['statistics']['viewCount'])
+
+    def __repr__(self):
+        return f"Channel(id={self.id}, title={self.title}, subscriber_count={self.subscriber_count})"
+
+    def __str__(self):
+        return f"{self.title} ({self.url}) - Subscribers: {self.subscriber_count}"
+
+    def __add__(self, other):
+        if isinstance(other, Channel):
+            return Channel(self.channel_id, f"{self.title} + {other.title}", "", self.subscriber_count + other.subscriber_count)
+        raise TypeError("Unsupported operand type for +")
+
+    def __sub__(self, other):
+        if isinstance(other, Channel):
+            return Channel(self.channel_id, f"{self.title} - {other.title}", "", self.subscriber_count - other.subscriber_count)
+        raise TypeError("Unsupported operand type for -")
+
+    def __eq__(self, other):
+        if isinstance(other, Channel):
+            return self.subscriber_count == other.subscriber_count
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, Channel):
+            return self.subscriber_count < other.subscriber_count
+        raise TypeError("Unsupported operand type for <")
+
+    def __le__(self, other):
+        if isinstance(other, Channel):
+            return self.subscriber_count <= other.subscriber_count
+        raise TypeError("Unsupported operand type for <=")
+
+    def __gt__(self, other):
+        if isinstance(other, Channel):
+            return self.subscriber_count > other.subscriber_count
+        raise TypeError("Unsupported operand type for >")
+
+    def __ge__(self, other):
+        if isinstance(other, Channel):
+            return self.subscriber_count >= other.subscriber_count
+        raise TypeError("Unsupported operand type for >=")
 
     def get_channel_data(self):
         youtube = self.get_service()
@@ -59,12 +100,6 @@ class Channel:
 
     @staticmethod
     def string_to_number(value: str) -> float:
-        """
-        Преобразует строку в число.
-
-        :param value: Строка, представляющая число.
-        :return: Преобразованное число.
-        """
         try:
             return int(float(value))
         except ValueError:
