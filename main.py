10bc7c21b22a4b1a11ce6235877d80da7dfaa546
Nikolay Yaroshenko <nikolayyaroshenko@MacBook-Pro-Nikolay.local>
2023-11-22 13:35:19 +0300

    15 next try

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from src.video import Video
-from src.video import PLVideo
+from src.video import Video, PLVideo
+
 
 if __name__ == '__main__':
-
     video1 = Video('AWX4JnAnjBE')
     video2 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
-    assert str(video1) == 'GIL в Python: зачем он нужен и как с этим жить'
-    assert str(video2) == 'MoscowPython Meetup 78 - вступление'
+    print(video1.title)
+    print(video2)
+    assert str(video1.title) == 'GIL в Python: зачем он нужен и как с этим жить'
+    assert str(video2.title) == 'MoscowPython Meetup 78 - вступление'
--- src/video.py
+++ src/video.py
@@ -1,10 +1,30 @@
+import requests
+
+
 class Video:
-    def __init__(self, video_id, title='', video_url='', views=0, likes=0):
+    def __init__(self, video_id):
         self.video_id = video_id
-        self.title = title
-        self.video_url = video_url
-        self.views = views
-        self.likes = likes
+        self.title = ""
+        self.video_url = ""
+        self.views = 0
+        self.likes = 0
+        self._load_video_info()
+
+    def _load_video_info(self):
+        api_key = "AIzaSyBgM_QMOxIgBaTz56iO5LSodb_L-vypyrk"
+        url = f"https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id={self.video_id}&key={api_key}"
+        response = requests.get(url)
+        data = response.json()
+
+        if "items" in data and data["items"]:
+            snippet = data["items"][0]["snippet"]
+            self.title = snippet.get("title", "")
+            self.video_url = f"https://www.youtube.com/watch?v={self.video_id}"
+
+        if "items" in data and data["items"]:
+            statistics = data["items"][0]["statistics"]
+            self.views = int(statistics.get("viewCount", 0))
+            self.likes = int(statistics.get("likeCount", 0))
 
     def __repr__(self):
         return f"Video(id={self.video_id}, title={self.title}, views={self.views}, likes={self.likes})"
@@ -14,8 +34,8 @@ class Video:
 
 
 class PLVideo(Video):
-    def __init__(self, video_id, playlist_id, title='', video_url='', views=0, likes=0):
-        super().__init__(video_id, title, video_url, views, likes)
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
         self.playlist_id = playlist_id
 
     def __repr__(self):
@@ -23,3 +43,6 @@ class PLVideo(Video):
 
     def __str__(self):
         return f"{self.title} ({self.video_url}) - Views: {self.views}, Likes: {self.likes}, Playlist ID: {self.playlist_id}"
+
+
+
